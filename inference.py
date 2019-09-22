ee1b8005086bfd02fdf4817ea559fd2c31b35a4f
Anton Okhotnikov <ant.okhotnikov@gmail.com>
2019-09-22 14:54:06 +0300

    feat: Add inference from url (still requires debugging)

--- inference.py
+++ inference.py
@@ -2,12 +2,17 @@
 
 import os
 import sys
+import tensorflow as tf
+from tensorflow import keras
 from keras.models import load_model
 from keras import backend as K
 import librosa
+import soundfile as sf
 import numpy as np
 import random
 import time
+import io
+from urllib.request import urlopen
 
 sys.path.append(os.path.abspath("data/src"))
 from DftSpectrogram import DftSpectrogram
@@ -15,16 +20,9 @@ from focal_loss import focal_loss
 K.clear_session()
 
 
-modelpath = '/home/anton/contests/boosters/deploy/web/data/model/model.h5'
-custom_objects = {'DftSpectrogram': DftSpectrogram, 'focal_loss_fixed': focal_loss()}
-
-model = load_model(modelpath, custom_objects=custom_objects)
-model.summary()
-
-
-def get_feature(wav_path, length=102000, random_start=False):
+def get_feature(wav_path, length=102800, random_start=False):
     try:
-        x, sr = librosa.load(wav_path, sr=None)
+        x, sr = librosa.load(wav_path, sr=16000)
         assert sr == 16000
         if length > len(x):
             x = np.concatenate([x] * int(np.ceil(length/len(x))))
@@ -37,8 +35,27 @@ def get_feature(wav_path, length=102000, random_start=False):
         return None
 
 
-def test_pred(model, filepath):
-    feature = get_feature(filepath, length=102800, random_start=True)
+def get_feature_from_url(wav_path, length=102800, random_start=False):
+    try:
+        # x, sr = librosa.load(wav_path, sr=16000)
+        x, sr = sf.read(io.BytesIO(urlopen(url).read()))
+        assert sr == 16000
+        if length > len(x):
+            x = np.concatenate([x] * int(np.ceil(length/len(x))))
+        if random_start:
+            x = x[random.randint(0, len(x) - length):]
+        feature = x[:length]
+        return feature / np.max(np.abs(feature))
+    except Exception as e:
+        print("Error with getting feature from %s: %s" % (wav_path, str(e)))
+        return None
+
+
+def test_pred(model, filepath, url=False):
+    if url:
+        feature = get_feature_from_url(filepath, length=102800, random_start=True)
+    else:
+        feature = get_feature(filepath, length=102800, random_start=True)
     transfeat = feature[np.newaxis, ..., np.newaxis]
     return model.predict(transfeat)
 
@@ -53,5 +70,13 @@ def run_tests(items=['data/test/test_2s.wav']):
         print("Time elapsed {spent:.4f}\n\n".format(spent=end - start))
     pass
 
-items = ['data/test/test_2s.wav', 'data/test/test_25s.wav', 'data/test/test_4s.wav', 'data/test/attack_19s.wav']
-run_tests(items)
+
+if __name__ == "__main__":
+    modelpath = '/home/anton/contests/boosters/deploy/web/data/model/model.h5'
+    custom_objects = {'DftSpectrogram': DftSpectrogram, 'focal_loss_fixed': focal_loss()}
+
+    model = load_model(modelpath, custom_objects=custom_objects)
+    model.summary()
+
+    items = ['data/test/test_2s.wav', 'data/test/test_25s.wav', 'data/test/test_4s.wav', 'data/test/attack_19s.wav']
+    run_tests(items)
